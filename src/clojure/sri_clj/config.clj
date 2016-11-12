bc96a87dff55a03260c359c43d262d111806b7b6
Eduardo Raad <eduardo.raad@datilmedia.com>
2016-11-12 12:27:13 -0500

    Sets up automatic environment configuration.

--- src/clojure/sri_clj/config.clj
+++ src/clojure/sri_clj/config.clj
@@ -1,1 +1,13 @@
-(ns sri-clj.config)
+(ns sri-clj.config
+  (:require [environ.core :as env]))
+
+(def local-conf (read-string (slurp "config/sri_envs.edn")))
+
+(def envs {:sri-validation-test (env/env :sri-validation-test
+                                         (:sri-validation-test local-conf))
+           :sri-validation-prod (env/env :sri-validation-prod
+                                         (:sri-validation-prod local-conf))
+           :sri-auhorization-test (env/env :sri-authorization-test
+                                           (:sri-authorization-test local-conf))
+           :sri-auhorization-prod (env/env :sri-authorization-prod
+                                           (:sri-authorization-prod local-conf))})
