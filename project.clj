0f4fbfb88b7d9015c8978a81832844b8ecc3eb39
Eduardo Raad <eduardo.raad@datilmedia.com>
2016-11-13 12:43:44 -0500

    Adds the datil namespace.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sri-clj "0.1.0"
+(defproject co.datil/sri-clj "0.1.0"
   :description "Librería Clojure para los servicios web del Servicio de Rentas Internas."
   :url "https://github.com/datil/sri-clj"
   :license {:name "Eclipse Public License"
